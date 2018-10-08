34229507b3e73e6383488f95bc89cc7b736170b5
Aler Denisov <aler.zampillo@gmail.com>
2018-10-08 06:17:48 +0100

    wip

---
+++ run.sh
@@ -0,0 +1,14 @@
+#! /bin/bash
+
+rm -rf data/eos/mnt
+rm -rf data/eos/mongo
+rm -rf data/parity/cache
+rm -rf data/parity/chains
+rm -rf data/parity/dapps
+rm -rf data/parity/network
+rm -rf data/parity/signer
+rm -rf data/parity/jsonrpc.json
+rm -rf data/rethinkdb
+
+docker-compose rm -v -f
+docker-compose up rethinkdb parity
