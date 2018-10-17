df1aad7f8de8d78b773364b83559a98ef8b0fdb4
Aler Denisov <aler.zampillo@gmail.com>
2018-10-17 13:13:46 +0300

    fix(eos): Remove dirty logs

--- src/eos/traces.ts
+++ src/eos/traces.ts
@@ -51,14 +51,11 @@ export async function run(ctx: Context) {
 
     cachedConnection = ctx.connection;
     stream.each(async (_, hash) => {
-      console.log(`New transaction ${hash}, request traces`);
       const trace = await TransactionTraceModel.findOne({ id: hash }).exec();
-      // const root = trace.action_traces;
       const traces = trace.action_traces
         .map((t, index) => flatRecursive([index], t))
         .reduce((flat, arr) => flat.concat(arr), []);
 
-      console.log(JSON.stringify(traces, null, 2));
       await ctx.tables.traces
         .insert(<any>traces, { conflict: 'update' })
         .run(ctx.connection);
