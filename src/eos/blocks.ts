5a5cfdfb755e0d63fe51dd7b1fbdf45528578e8e
Aler Denisov <aler.zampillo@gmail.com>
2018-10-17 13:13:07 +0300

    feat(eos): Add block grabber script

--- src/eos/blocks.ts
+++ src/eos/blocks.ts
@@ -31,52 +31,9 @@ export async function run(ctx: Context) {
     transactionsRoot: block.block.transaction_mroot,
   }));
 
-  // const eosTrx = blocks
-  //   .filter(block => block.block.transactions.length > 0)
-  //   .map(block =>
-  //     block.block.transactions.map(
-  //       (trx, index) => ((trx.block = block), (trx.index = index), trx),
-  //     ),
-  //   )
-  //   .reduce((flat, arr) => flat.concat(arr), [])
-  //   .map<IEosTransaction>(trx => ({
-  //     txHash: trx.trx.id,
-  //     blockHash: trx.block.block_id,
-  //     blockHeight: trx.block.block_num,
-  //     input: trx.trx.packed_trx,
-  //     recipient: trx.trx.transaction.actions[0].account,
-  //     sender: trx.trx.transaction.actions[0].authorization[0].actor,
-  //     signature: trx.trx.signatures.join(','),
-  //     timestamp: trx.block.createdAt.getTime() / 1000,
-  //     txIndex: trx.index,
-  //   }));
-  // console.log(eosTrx);
-
-  // console.log(`EOS Blocks ${eosBlocks.length}`);
-
   await ctx.tables.blocks
     .insert(<any>eosBlocks, {
       conflict: 'replace',
     })
     .run(ctx.connection);
-
-  // await ctx.tables.trx
-  //   .insert(<any>eosTrx, {
-  //     conflict: 'error',
-  //   })
-  // .run(ctx.connection);
-
-  // console.log(
-  //   blocks.reduce(
-  //     (sum, block) => ((sum += block.block.transactions.length), sum),
-  //     0,
-  //   ),
-  // );
-
-  // console.log(
-  //   blocks
-  //     .filter(block => block.block.transactions.length)
-  //     .map(block => block.block.transactions)
-  //     .reduce((flat, arr) => flat.concat(arr), []),
-  // );
 }
